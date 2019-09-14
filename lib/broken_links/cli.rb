f9b445571b10bfe1a422655d72aa301ecae4fae6
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2019-09-13 23:11:18 -0300

    Adds params

--- lib/broken_links/cli.rb
+++ lib/broken_links/cli.rb
@@ -5,14 +5,21 @@ require 'broken_links'
 
 module BrokenLinks
   class CLI < Thor
-    desc 'find', 'Finds links recursively'
+    desc '', 'Finds links recursively'
     method_option :url, aliases: '-u', required: true, type: :string
+    method_option :json, default: false, type: :boolean
+    method_option :print, aliases: '--p', default: true, type: :boolean
     method_option :depth, aliases: '-d', default: 10, type: :numeric
+
     def find
       url = options[:url]
-      depth = options[:url]
+      depth = options[:depth]
+      o_print = options[:print]
+      json = options[:json]
 
-      puts BrokenLinks::Crawler.new(url: url, depth: depth)
+      puts BrokenLinks::Crawler.new(url: url, depth: depth, print: o_print, json: json).start
     end
+
+    default_task :find
   end
 end
